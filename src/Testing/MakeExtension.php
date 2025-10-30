d9017b29fa07a46c1e1500a62de0386ce8529df7
William Correa <wilcorrea@gmail.com>
2025-10-30 11:00:09 -0300

    feat(testing): add named arguments support to MakeExtension
    - Support both named and positional arguments for better clarity
    - Maintain backward compatibility with existing tests
    - Named arguments take priority over positional arguments
    - Refactor to use match expression for cleaner code
    - Remove inline comments as per project conventions

--- src/Testing/MakeExtension.php
+++ src/Testing/MakeExtension.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Constructo\Testing;
 
+use InvalidArgumentException;
 use ReflectionClass;
 use ReflectionException;
+use ReflectionParameter;
 
 /**
  * @phpstan-ignore trait.unused
@@ -22,6 +24,38 @@ trait MakeExtension
      */
     protected function make(string $class, array $args = []): object
     {
-        return (new ReflectionClass($class))->newInstanceArgs(array_values($args));
+        $reflection = new ReflectionClass($class);
+        $constructor = $reflection->getConstructor();
+
+        if ($constructor === null) {
+            return $reflection->newInstance();
+        }
+
+        $orderedArgs = [];
+        $position = 0;
+        foreach ($constructor->getParameters() as $param) {
+            $orderedArgs[] = $this->resolveParameter($param, $args, $class, $position);
+            $position++;
+        }
+
+        return $reflection->newInstanceArgs($orderedArgs);
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    private function resolveParameter(ReflectionParameter $param, array $args, string $class, int $position): mixed
+    {
+        $name = $param->getName();
+
+        return match (true) {
+            array_key_exists($name, $args) => $args[$name],
+            array_key_exists($position, $args) => $args[$position],
+            $param->isDefaultValueAvailable() => $param->getDefaultValue(),
+            $param->allowsNull() => null,
+            default => throw new InvalidArgumentException(
+                sprintf('Missing required parameter: $%s for class %s', $name, $class)
+            ),
+        };
     }
 }
